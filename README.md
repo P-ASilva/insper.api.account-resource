219cabda1f74db0387b3a8c4b009c54cea19b5b9
P-ASilva <pedroas5.al.insper.edu>
2024-03-11 10:15:48 -0300

    base setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # insper.api.account.resource
+
 2024.1 BCC 
+
+Service for a account system
+
+run with - ```mvn clean package:springboot run```
---
+++ src/main/java/insper/api/account/Account.java
@@ -0,0 +1,20 @@
+package insper.api.account;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@Builder
+@Getter @Setter @Accessors(fluent = true, chain = true)
+@AllArgsConstructor @NoArgsConstructor
+public class Account {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String hash;
+}
